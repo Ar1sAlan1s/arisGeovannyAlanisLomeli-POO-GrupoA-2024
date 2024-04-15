d309951bfae17182a53491e5d0223c143bdb66a1
Ar1sAlan1s <159462419+Ar1sAlan1s@users.noreply.github.com>
2024-04-14 21:19:00 -0600

    Add files via upload

---
+++ src/Usuarios/Gerente.java
@@ -0,0 +1,20 @@
+package Usuarios;
+
+import Usuarios.utils.Rol;
+
+import java.time.LocalDate;
+
+
+public class Gerente extends Usuario {
+
+    private double sueldo;
+    public Gerente(String nombre, String apellido, Rol rol,LocalDate fechedenacimiento,String usuario,String contraseña) {
+        super(nombre,apellido,Rol.GERENTE,usuario,contraseña,fechedenacimiento);
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, Numero de usuarios: %s",super.toString(),cantidadUsuarios-2);
+    }
+}
